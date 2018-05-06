05e888a3f72a8a3f9a254bd3ab5ad4409c9ddb2b
ILIAN LONDOÑO LONDOÑO <ilian.londono@correo.icesi.edu.co>
2018-05-06 15:05:07 -0500

    mi primer commit

---
+++ index.js
@@ -0,0 +1,50 @@
+$(function () {
+    var header = document.getElementById('header');
+    var headroom = new Headroom(header);
+    headroom.init();
+
+    //Menu Responsive
+
+    //Calculamos ancho de pagina
+
+    var ancho = $(window).width(),
+        enlaces = $('#enlaces'),
+        nonav = $('#cont-nonav'),
+        btnMenu = $('#btn-menu'),
+        icono = $('#btn-menu .icono');
+
+    if (ancho < 908) {
+        enlaces.hide();
+        nonav.hide();
+        icono.addClass('fa-bars');
+
+    }
+
+    btnMenu.on('click', function (e) {
+        enlaces.slideToggle();
+        nonav.slideToggle();
+        icono.toggleClass('fa-bars');
+        icono.toggleClass('fa-times');
+    });
+
+    $(window).on('resize', function () {
+        if ($(this).width() > 908) {
+            enlaces.show();
+            nonav.show();
+            icono.addClass('fa-times');
+            icono.removeClass('fa-bars');
+        } else {
+            enlaces.hide();
+            nonav.hide();
+            icono.addClass('fa-bars');
+            icono.removeClass('fa-times');
+        }
+    });
+
+    var colores = document.getElementById('color-wheel');
+   // var colorLuces = document.getElementsByClassName('colorFondoimg');
+    $('#color-wheel').wheelColorPicker();
+    $('#color-wheel').on('slidermove', function(e) {
+        document.querySelector('#fondoCol').style.backgroundColor = "#"+colores.value;
+    });
+});
