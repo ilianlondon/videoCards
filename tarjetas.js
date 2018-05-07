097abf25aee4e8512da20d77d7d191491b7207ba
ILIAN LONDOÑO LONDOÑO <ilian.londono@correo.icesi.edu.co>
2018-05-06 21:06:08 -0500

    Intento de visualizacion de base de datos

--- tarjetas.js
+++ tarjetas.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 MongoClient.connect('mongodb://localhost:27017', function (err, client) {
 if (err) throw err;
 
-db = client.db('videocards');
+db = client.db('test');
 
 // Iniciar servidor
 app.listen(5000);
@@ -61,7 +61,7 @@ app.get('/', (req, res) => {
     prod.toArray((err, result) => {
         console.log(result);
             res.render('products', {
-                juego: result
+                videocards: result
             });
         })
 });
