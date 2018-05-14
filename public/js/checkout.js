c211708f06f9dfa6ed2d8c2428f922dfc47b64b6
ILIAN LONDOÑO LONDOÑO <ilian.londono@correo.icesi.edu.co>
2018-05-13 23:58:49 -0500

    check out completado

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -10,6 +10,6 @@ fetch('http://localhost:5000/productosPorId?id='+arreglo)
 
     var lista = document.querySelector('.lista');
     res.forEach(function(elem){
-        lista.innerHTML += '<li><img width="100" src="'+elem.image+'">' + elem.name + '</li>';
+        lista.innerHTML += '<li class="tarjeta-check"><div class="cont-tarjeta-check"><div class="img-tarjeta-check"><img width="100" src="' + elem.image + '" class="imagen-tarj-check"></div><div class="continfo"><p class="name-check">' + elem.name + " " + elem.memory + '</p><p class="price-check"><span>$</span> ' + elem.price + '</p></div></div></li><div class="divide-prod-check"></div>';
     });
 });
